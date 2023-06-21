cd4d45391798c72a3afb96bfe3fc19e2c8a99ef1
mibal_ua <9mohapx9@gmail.com>
2023-06-21 14:22:03 +0200

    Implement `QrActivity` and it `xml`

--- app/src/main/java/com/example/sunlakesEatingTracker/QrActivity.java
+++ app/src/main/java/com/example/sunlakesEatingTracker/QrActivity.java
@@ -16,15 +16,126 @@
 
 package com.example.sunlakesEatingTracker;
 
-import androidx.appcompat.app.AppCompatActivity;
+import static android.Manifest.permission.CAMERA;
+import static android.content.pm.PackageManager.PERMISSION_GRANTED;
 
 import android.os.Bundle;
+import android.util.SparseArray;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+
+import java.io.IOException;
 
 public class QrActivity extends AppCompatActivity {
+
+    private static final int REQUEST_CAMERA_PERMISSION = 201;
+
+    private SurfaceView surfaceView;
+    private TextView textViewScannedValue;
+
+    private BarcodeDetector barcodeDetector;
+    private CameraSource cameraSource;
+    private String intentData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qr);
+
+        initViews();
+    }
+
+    private void initViews() {
+        textViewScannedValue = findViewById(R.id.textViewScannedValue);
+        surfaceView = findViewById(R.id.surfaceView);
+    }
+
+    private void initialiseDetectorsAndSources() {
+        Toast.makeText(getApplicationContext(),
+                "Barcode scanner started",
+                Toast.LENGTH_SHORT
+        ).show();
+
+        barcodeDetector = new BarcodeDetector.Builder(this)
+                .setBarcodeFormats(Barcode.ALL_FORMATS)
+                .build();
+        cameraSource = new CameraSource.Builder(this, barcodeDetector)
+                .setRequestedPreviewSize(1920, 1080)
+                .setAutoFocusEnabled(true)
+                .build();
+        surfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
+
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+                try {
+                    if (ActivityCompat.checkSelfPermission(QrActivity.this, CAMERA) == PERMISSION_GRANTED) {
+                        cameraSource.start(surfaceView.getHolder());
+                    } else {
+                        ActivityCompat.requestPermissions(
+                                QrActivity.this,
+                                new String[]{CAMERA},
+                                REQUEST_CAMERA_PERMISSION
+                        );
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+                cameraSource.stop();
+            }
+        });
+        barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
+            @Override
+            public void release() {
+                Toast.makeText(getApplicationContext(),
+                        "To prevent memory leaks barcode scanner has been stopped",
+                        Toast.LENGTH_SHORT
+                ).show();
+            }
+
+            @Override
+            public void receiveDetections(@NonNull Detector.Detections<Barcode> detections) {
+                final SparseArray<Barcode> barcodes = detections.getDetectedItems();
+                textViewScannedValue.post(() -> {
+                    if (barcodes.size() == 0) {
+                        textViewScannedValue.setText("No DATA");
+                        return;
+                    }
+                    // TODO send request  
+                    intentData = barcodes.valueAt(0).displayValue;
+                    textViewScannedValue.setText(intentData);
+                });
+            }
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        cameraSource.release();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        initialiseDetectorsAndSources();
     }
 }
